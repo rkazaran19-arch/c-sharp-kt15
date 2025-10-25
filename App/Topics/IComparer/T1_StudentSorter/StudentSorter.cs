cb9bb43b02588bd88f29479c8e5168500de17856
user1 <user1@DESKTOP-ENRJJP4>
2025-10-25 15:48:03 +0300

    task  1

--- App/Topics/IComparer/T1_StudentSorter/StudentSorter.cs
+++ App/Topics/IComparer/T1_StudentSorter/StudentSorter.cs
@@ -1,1 +1,38 @@
 namespace App.Topics.IComparer.T1_StudentSorter;
+
+public record Student(string LastName, int Age, double Gpa);
+
+public class StudentMultiComparer : IComparer<Student>
+{
+    int IComparer<Student>.Compare(Student? x, Student? y)
+    {
+        if (x is null && y is null)
+            return 0;
+        if (x is null)
+            return -1;
+        if (y is null)
+            return 1;
+
+        int LN = StringComparer.OrdinalIgnoreCase.Compare(x.LastName, y.LastName);
+        if (LN != 0)
+            return LN;
+
+        int newage = y.Age.CompareTo(x.Age);
+        if (newage != 0)
+            return newage;
+
+        return y.Gpa.CompareTo(x.Gpa);
+    }
+}
+
+public static class StudentSorter
+{
+    public static IEnumerable<Student> Sort(IEnumerable<Student> students)
+    {
+        if (students == null)
+            return Enumerable.Empty<Student>();
+
+        return students.OrderBy(s => s.LastName, StringComparer.OrdinalIgnoreCase).ThenByDescending(s => s.Age).ThenByDescending(s => s.Gpa);
+    }
+}
+
